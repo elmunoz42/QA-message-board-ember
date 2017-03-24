eac85dc745fd60f8b7dee56290c7179a2ca5eb83
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-24 11:48:21 -0700

    edit update actions in question.js and add dynamic route link to question dynamic page.

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
-  }
+  },
+  update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    }
 });
