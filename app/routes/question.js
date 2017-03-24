585ea4fb10d8dbdcf3a42f9b17ec6ca9834a8752
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-24 14:05:16 -0700

    integrate functionality to display answers of a given question.

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
 //   model(params) {
 //   return Ember.RSVP.hash({
 //     question: this.store.findRecord('question', params.question_id),
